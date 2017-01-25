ad360df8273565c8945ebb37599c6ed2bf1bbd32
powerfulech0 <powerfulech0development@gmail.com>
2017-01-25 13:25:03 -0500

    Added Navigation Features

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function newProjectWindow() {
-  window.open("https://powerfulech0.github.io/supermegashuffle");
+  	window.open("https://powerfulech0.github.io/supermegashuffle");
 }
+
+$("#portfoliotab").on("click", function() {
+	$(".active").removeClass("active");
+	$("#portfoliotab").addClass("active");
+});
+
+$("#aboutmetab").on("click", function() {
+	$(".active").removeClass("active");
+	$("#aboutmetab").addClass("active");
+});
