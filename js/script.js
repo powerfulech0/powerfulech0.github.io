e4a6174177739f808cec596c639eced536b3abf0
powerfulech0 <powerfulech0development@gmail.com>
2017-01-25 13:59:00 -0500

    Added Link Properties

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-function newProjectWindow() {
-  	window.open("https://powerfulech0.github.io/supermegashuffle");
+var supermegashuffle = "https://powerfulech0.github.io/supermegashuffle";
+var supermega = "https://www.youtube.com/channel/UCPPc2PdtA7gCMbjYp_i_TKA";
+var github = "https://github.com/powerfulech0";
+var frandios = "https://onsen.io/blog/onsen-ui-youtube-api-v3/";
+
+function newProjectWindow(url) {
+  	window.open(url);
 }
+
+$(".supermegashufflelink").on("click", function() {
+	newProjectWindow(supermegashuffle);
+});
+
+$(".navbar-brand").on("click", function() {
+	newProjectWindow(github);
+});
+
+$("#supermega").on("click", function() {
+	newProjectWindow(supermega);
+})
+
+$("#frandios").on("click", function() {
+	newProjectWindow(frandios);
+})
 
 $("#portfoliotab").on("click", function() {
 	$(".active").removeClass("active");
