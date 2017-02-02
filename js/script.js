e5224b2fc70eb3960039ae8d359b9a53ccf5a745
powerfulech0 <powerfulech0development@gmail.com>
2017-02-02 13:24:35 -0500

    Creating Contact Section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var supermegashuffle = "https://powerfulech0.github.io/supermegashuffle";
 var supermega = "https://www.youtube.com/channel/UCPPc2PdtA7gCMbjYp_i_TKA";
 var github = "https://github.com/powerfulech0";
+var linkedin = "https://www.linkedin.com/in/gregoryrearden/";
 var frandios = "https://onsen.io/blog/onsen-ui-youtube-api-v3/";
 
 function newProjectWindow(url) {
@@ -11,17 +12,21 @@ $(".supermegashufflelink").on("click", function() {
 	newProjectWindow(supermegashuffle);
 });
 
-$(".navbar-brand").on("click", function() {
-	newProjectWindow(github);
-});
-
 $("#supermega").on("click", function() {
 	newProjectWindow(supermega);
-})
+});
 
 $("#frandios").on("click", function() {
 	newProjectWindow(frandios);
-})
+});
+
+$("#github").on("click", function() {
+	newProjectWindow(github);
+});
+
+$("#linkedin").on("click", function() {
+	newProjectWindow(linkedin);
+});
 
 $("#portfoliotab").on("click", function() {
 	$(".active").removeClass("active");
@@ -31,4 +36,9 @@ $("#portfoliotab").on("click", function() {
 $("#aboutmetab").on("click", function() {
 	$(".active").removeClass("active");
 	$("#aboutmetab").addClass("active");
+});
+
+$("#contacttab").on("click", function() {
+	$(".active").removeClass("active");
+	$("#contacttab").addClass("active");
 });
